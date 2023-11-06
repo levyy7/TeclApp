94a08d12e0359529c255a16f5414c65d91bb3dab
eneko.sabate <eneko.sabate@estudiantat.upc.edu>
2023-11-06 11:19:36 +0100

    esqueleto compilable

---
+++ QAPBasic.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+
+public class QAPBasic implements EstrategiaCreacionLayout {
+    
+    
+    public int[] crearLayout(
+        double[][] distLoc,
+        double[][] traficoInst) {
+
+        
+        return new int[0];
+    }
+
+}
