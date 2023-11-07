f170432484672533d101ae819f076413da66a73f
pol.ribera <pol.ribera@estudiantat.upc.edu>
2023-11-07 11:36:24 +0100

    probablement no funciona

---
+++ make.bat
@@ -0,0 +1,7 @@
+javac --release 11 dominio/*.java 
+javac --release 11 algoritmios/*.java
+javac --release 11 controladores/*.java 
+javac --release 11 docs/*.java 
+javac --release 11 drivers/*.java 
+javac --release 11 Exceptions/*.java
+javac --release 11 *.java
