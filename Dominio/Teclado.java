fa001c842e493fcd6939c7e62782ceec93dce768
mariona.aguilera <mariona.aguilera@estudiantat.upc.edu>
2023-11-14 13:27:20 +0100

    entradas

---
+++ Dominio/Teclado.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.awt.Point;
+
+public class Teclado {
+    String nombre;
+    String algoritmo;
+    String alfabeto;
+    Map<Character, Point> distribucion = new HashMap<>();
+
+
+    public Teclado(String nombre, String algoritmo, String alfabeto, Map<Character, Point> distribucion) {
+        this.nombre = nombre;
+        this.algoritmo = algoritmo;
+        this.alfabeto = alfabeto;
+        this.distribucion = distribucion;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String algoritmo(){
+        return this.algoritmo;
+    }
+
+    public String alfabeto(){
+        return this.alfabeto;
+    }
+
+    public Map<Character, Point> getDistribucion(){
+        return this.distribucion;
+    } 
+
+    public void setNombre(String newName){
+        this.nombre = newName;
+    }
+
+    public void setAlgoritmo(String newAlgoritmo){
+        this.nombre = newAlgoritmo;
+    }
+
+    public void setAlfabeto(String newAlfabeto) {
+        this.alfabeto = newAlfabeto;
+    }
+
+    public void setDistribucion(Map<Character, Point> newDist){
+        this.distribucion = newDist;
+    }
+
+    public void modify(Map<Character, Point> newDist){
+        this.distribucion = newDist;
+    }
+}
+
+//mariona
