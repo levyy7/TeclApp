192a02298c26e1e262ba7df9c9e6e401396f9530
Miguel <miguel.angel.montero@estudiantat.upc.edu>
2023-11-17 17:03:20 +0100

    BorradoDeMake.sh

--- make.bat
+++
@@ -1,3 +0,0 @@
-javac --release 11 src/main/dominio/*.java
-javac --release 11 src/main/dominio/algoritmos/*.java
-javac --release 11 src/drivers/*.java
--- src/make.sh
+++
@@ -1,3 +0,0 @@
-javac --release 11 main/dominio/*.java ;
-javac --release 11 main/dominio/algoritmos/*.java ;
-javac --release 11 drivers/*.java ;
