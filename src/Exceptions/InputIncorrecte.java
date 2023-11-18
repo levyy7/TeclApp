c43a491364c72a47e10c4712958c83d86a21705e
pol.ribera <pol.ribera@estudiantat.upc.edu>
2023-11-18 14:53:54 +0100

    compila Entrada

---
+++ src/Exceptions/InputIncorrecte.java
@@ -0,0 +1,14 @@
+package Excepcions;
+
+public class InputIncorrecte extends ExcepcionsCentral{
+
+    public String getTipus() {
+        return "InputIncorrecte";
+    }
+
+    public InputIncorrecte() {
+        super("Imput incorrecte tontito");
+    }
+    
+    
+}
--- src/Exceptions/InputJaCreat.java
+++ src/Exceptions/InputJaCreat.java
@@ -1,13 +1,13 @@
 package Excepcions;
 
-public class InputJaCreat extends ExcepcionsRecomanador{
+public class InputJaCreat extends ExcepcionsCentral{
 
     public String getTipus() {
         return "InputJaCreat";
     }
 
-    public InputJaCreat() {
-        super("");
+    public InputJaCreat(String s) {
+        super("ja existeix un imput amb el nom:"+s);
     }
     
 }
